1c7686422c506bf0c6b421db0adcb3fe228d832b
MD. Mahadee Hassan <mhassan582@gmail.com>
2023-05-17 18:19:44 +0600

    Header complete
    Home layout placed

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,47 +1,77 @@
 import React from 'react';
-import {Nav, Navbar, NavDropdown, Container} from 'react-bootstrap';
+import {Nav, Navbar, Container, Form, InputGroup} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import {LinkContainer} from 'react-router-bootstrap';
-import logo from '../../assets/images/logo/logo.png';
+import logo from '../../assets/images/logo/penlagbe-logo.png';
 
 export default function NavigationHeader() {
+  //States
+  const [searchShow, setSearchShow] = React.useState(false);
+  //Function
+  const handleSearch = () => {
+    setSearchShow(!searchShow);
+  };
   return (
     <>
       <div className='container' >
-        <div className='site-brand'>
-          <Link to='/'>
-            <img src={logo} alt='brand logo' height='100px' />
-          </Link>
-          <span>
-            <i></i>
-          </span>
+        <div className='row'>
+          <div className='col-4 header-left'>
+            <div>
+              <PrimaryNavbar cid={'drawer-navbar'} header={false} />
+            </div>
+            
+          </div>
+          <div className='site-brand col-4'>
+            <Link to='/'>
+              <img src={logo} 
+                alt='brand logo' className='logo-image'/>
+            </Link>
+          </div>
+          <div className='col-4 header-right'>
+            {searchShow ? 
+              <InputGroup className='search-bar'>
+                <Form.Control
+                  placeholder="Search"
+                />
+                <InputGroup.Text onClick={handleSearch}>
+                  <i className="fa-sharp fa-solid fa-xmark"></i>
+                </InputGroup.Text>
+              </InputGroup> : 
+              <i onClick={handleSearch} 
+                className="fa-sharp fa-solid fa-magnifying-glass"></i>}
+            <i className="fa-sharp fa-solid fa-cart-shopping"
+              style={{marginLeft: '12px'}}></i>
+          </div>
+          
         </div>
       </div>
-      <Navbar bg="white" className='header-border'>
-        <Container className="justify-content-center">
-          <Nav>
-            <LinkContainer to="/">
-              <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Something
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
+      <div>
+        <PrimaryNavbar cid={'header-navbar'} header={true} />
+      </div>
     </>
+  );
+}
+
+function PrimaryNavbar({cid, header}) {
+  return (
+    <Navbar id={cid} bg="white" expand="lg" 
+      className={header ? 'header-border' : ''}>
+      <Container>
+          
+        <Navbar.Toggle aria-controls="nav-toggle" />
+        <Navbar.Collapse 
+          className="justify-content-center" 
+          id="nav-toggle">
+          <Nav className="justify-content-center">
+            <LinkContainer to='/'>
+              <Nav.Link>Shop</Nav.Link>
+            </LinkContainer>
+            <Nav.Link href="#link">My Wishlist</Nav.Link>
+            <Nav.Link href="#link">Compare</Nav.Link>
+            <Nav.Link href="#link">My Account</Nav.Link>       
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import cover from '../../assets/images/covers/pxfuel.jpg';
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-2'>
+            ksfkjsdbkajdbvkajbvk
+          </div>
+          <div className='col-8'>
+            <img src={cover} alt='Background cover' className='bg-cover' />
+          </div>
+          <div className='col-2'>
+            akhsbdjahbsdj
+          </div>
+        </div>
+      </div>
     </>
   );
 }
